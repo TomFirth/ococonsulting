8a62acf2fe40363cc782095af363a5baa62bba79
Tom Firth <tom.firth84@gmail.com>
2020-05-03 16:53:40 +0100

    I always forget Heroku's port

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ var http = require('http'),
 fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
-    }       
+		}     
+		const port = process.env.PORT || 8000;  
     http.createServer(function(req, res) {  
         res.writeHeader(200, {"Content-Type": "text/html"});  
         res.write(html);  
         res.end();  
-    }).listen(8000);
+    }).listen(port);
 });
